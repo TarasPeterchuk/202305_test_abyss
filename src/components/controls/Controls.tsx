0c203ea43155248f8b92d1f395034088bf335d3f
apache0505 <jiayulutsk@ukr.net>
2023-05-18 17:09:18 +0300

    фі

--- src/components/controls/Controls.tsx
+++ src/components/controls/Controls.tsx
@@ -21,7 +21,7 @@ const Controls: React.FC<ControlsProps> = ({
 }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  useEffect(() => onCenterBoard());
+  useEffect(() => onCenterBoard(), []);
   const handleNumberClick = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
